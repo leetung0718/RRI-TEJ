ff5f3f26a690525225a1a66a80df88cca33784a8
Tung Lee <tunglee@TungdeMacBook-Pro.local>
2023-02-02 15:54:50 +0800

    init

---
+++ 0config.py
@@ -0,0 +1,10 @@
+# Severity Dict
+# Usage: df['severity'] = df['severity'].apply(lambda x: severityDict[x])
+severityDict = {
+    -5: 5,
+    -4: 4,
+    -3: 3,
+    -2: 2,
+    -1: 1,
+    0: 0
+}
